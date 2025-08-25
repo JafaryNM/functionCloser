f1fd311edfcb2b99667f23b95913541bf09775b9
JAFARY <jaffarymdegela@gmail.com>
2025-08-25 19:40:03 +0300

    functon return function

--- script.js
+++ script.js
@@ -48,29 +48,48 @@
 // newPassport(jafary);
 // checkIn(flight, jafary);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...otherWords] = str.split(' ');
+//   return [first.toUpperCase(), ...otherWords].join(' ');
+// };
+
+// // Higher Order function
+// const transformer = function (str, fn) {
+//   console.log(`Original String ${str}`);
+//   console.log(`Transformed string ${fn(str)}`);
+//   console.log(`Transformed method ${fn.name}`);
+// };
+
+// transformer('Javascript is the best', upperFirstWord);
+// transformer('Javascript is the best', oneWord);
+
+// // Javascript using call back all the time
+
+// const high5 = function () {
+//   console.log('Hi there');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// Function return function
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} , ${name}`);
+  };
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...otherWords] = str.split(' ');
-  return [first.toUpperCase(), ...otherWords].join(' ');
-};
+greet('Hellow')('Mosses');
+const greaterHey = greet('Hey');
+greaterHey('Jonas');
+greaterHey('Jafary');
 
-// Higher Order function
-const transformer = function (str, fn) {
-  console.log(`Original String ${str}`);
-  console.log(`Transformed string ${fn(str)}`);
-  console.log(`Transformed method ${fn.name}`);
-};
+// Change in arrow function
 
-transformer('Javascript is the best', upperFirstWord);
-transformer('Javascript is the best', oneWord);
+const greetArr = greeting => name => console.log(`${greeting}, ${name}`);
 
-// Javascript using call back all the time
-
-const high5 = function () {
-  console.log('Hi there');
-};
-
-document.body.addEventListener('click', high5);
+greetArr('JaffaryArrow')('Testing');
